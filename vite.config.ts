f7ef9950eebdd093f945d28e43103e76b3f195b9
alsaxvip <anniisarohmah@gmail.com>
2025-06-14 20:02:18 +0700

    Add files via upload

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,11 @@ import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    commonjsOptions: {
+      transformMixedEsModules: true,
+    },
+  },
   plugins: [react()],
   resolve: {
     alias: {
